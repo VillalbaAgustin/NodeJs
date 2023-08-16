09fe952446ce40c2462ad6414ad24de6c3405445
Villalba Agustin <villalba.agus.99@gmail.com>
2023-08-15 22:58:39 -0300

    first commit Clase-4

---
+++ clase-4/app.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const cors = require("cors");
+const crypto = require("node:crypto");
+const movies = require("./movies.json");
+const { validateMovie, validatePartialMovie } = require("./schemas/movies ");
+
+const PORT = process.env.PORT ?? 3000;
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        "http://localhost:8080",
+        "http://localhost:3000",
+        "https://movies.com",
+        "https://midu.dev",
+      ];
+
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true);
+      }
+
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
+
+app.disable("x-powered-by");
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hola mundo" });
+});
+
+// Todos los recursos que sean MOVIES se identifican con '/movies'
+app.get("/movies", (req, res) => {
+  const { genre } = req.query;
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      // movie.genre.includes(genre)      //! Es key sensitive (para este caso nos es indiferente)
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    );
+    return res.json(filteredMovies);
+  }
+
+  res.json(movies);
+});
+
+// Buscamos pelicula por ID
+app.get("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movie = movies.find((movie) => movie.id === id);
+
+  if (movie) {
+    return res.json(movie);
+  }
+
+  res.status(404).json({ message: "Movie not found" });
+});
+
+app.post("/movies", (req, res) => {
+  // const { title, year, director, duration, poster, genre, rate } = req.body;
+
+  const result = validateMovie(req.body);
+
+  if (result.error) {
+    res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(), // Crea un id versión 4
+    ...result.data, // Es correcto desestructurar pq ya paso por la validaciones !== ...data.body
+  };
+
+  movies.push(newMovie);
+
+  res.status(201).json(newMovie);
+});
+
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
+
+app.patch("/movies/:id", (req, res) => {
+  const result = validatePartialMovie(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data,
+  };
+
+  movies[movieIndex] = updateMovie;
+
+  return res.json(updateMovie);
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server listening con port: http://localhost:${PORT}`);
+});
---
+++ clase-4/schemas/movies .js
@@ -0,0 +1,43 @@
+const z = require("zod");
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: "Movie title must be a string",
+    required_error: "Movie title is required.",
+  }),
+  year: z.number().int().positive(),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10, { message: "The max rate is 10" }),
+  poster: z.string().url({ message: "Poster must be a calid URL" }),
+  genre: z.array(
+    z.enum([
+      "Action",
+      "Adventure",
+      "Crime",
+      "Comedy",
+      "Drama",
+      "Fantasy",
+      "Horror",
+      "Thriller",
+      "Sci-Fi",
+    ]),
+    {
+      required_error: "Movie genre is required.",
+      invalid_type_error: "Movie genre must be an array of enum Genre",
+    }
+  ),
+});
+
+const validateMovie = (object) => {
+  return movieSchema.safeParse(object)
+}
+
+const validatePartialMovie = (object) => {
+  return movieSchema.partial().safeParse(object)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
